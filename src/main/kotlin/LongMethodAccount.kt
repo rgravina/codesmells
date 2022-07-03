e2da1fa871850f977a3e5e8d093d378bc9a83221
Robert Gravina <robert.gravina@gmail.com>
2022-07-03 11:42:11 +0900

    Move comments to same line as code to make it clearer they are notes not code comments.

--- src/main/kotlin/LongMethodAccount.kt
+++ src/main/kotlin/LongMethodAccount.kt
@@ -4,12 +4,15 @@ class LongMethodAccount {
     private var open = true
     private var balance = 0
 
+    // There are a few steps in this method, but it's not clear where one starts and ends.
+    // In practice, you may see a lot of code like this. To the person who wrote it at the time, it might look OK.
+    // But to someone looking at it for the first time, you have to read the whole function to understand what it does.
     fun balance(): Int {
-        if (!open) {
+        if (!open) {                                                // Seems like some precondition checking.
             throw AccountNotOpenException()
         }
-        println("customer requested account balance")
-        if (balance < 0) {
+        println("customer requested account balance")               // Why is this println here? Was it for debugging?
+        if (balance < 0) {                                          // There is some interesting logic here
             // hide this from the customers
             return 0
         }
@@ -24,9 +27,9 @@ class LongMethodAccountRefactored {
     private var balance = 0
 
     fun balance(): Int {
-        ensureAccountIsOpen()
-        logBalanceRequest()
-        return customerFacingBalance()
+        ensureAccountIsOpen()                           // Now it's clear that there is application logic here
+        logBalanceRequest()                             // The println was really an important logging step we need
+        return customerFacingBalance()                  // The logic was extracted to a function with a good name
     }
 
     private fun ensureAccountIsOpen() {
@@ -39,9 +42,9 @@ class LongMethodAccountRefactored {
         println("customer requested account balance")
     }
 
-    private fun customerFacingBalance(): Int {
-        if (balance < 0) {
-            // hide this from the customers
+    private fun customerFacingBalance(): Int {          // So now we can see that the logic is to get the customer facing balance
+        if (balance < 0) {                              // The comments are still required to understand it, so it's a sign more refactoring is needed
+            // hide this from the customers             // Comments in the code are also another code smell. It's better to use descriptive names if possible.
             return 0
         }
         // ok, this is the normal case
--- src/main/kotlin/MysteriousName.kt
+++ src/main/kotlin/MysteriousName.kt
@@ -7,27 +7,19 @@
   Don't underestimate the value of a good name.
  */
 class MysteriousName {
-    // What is being exchanged from and to?
-    // What is value here?
-    fun exchange(value: Int): Double {
-        // What does new value mean?
-        // What is this constant?
-        val newValue = value / 135.0
-        // Do we need this temporary variable?
-        return newValue
+    fun exchange(value: Int): Double {                      // What is being exchanged from and to, and what is value?
+        val newValue = value / 135.0                        // What does new value mean? What is this constant?
+        return newValue                                     // Do we need this temporary variable?
     }
 }
 
 class MysteriousNameRefactored {
-    // The constant is now clearly the JPY to USD rate
-    private val jpyToUsdRate = 135.0
+    private val jpyToUsdRate = 135.0                            // The constant is now clearly the JPY to USD rate
 
-    // This function exchanges JPY to USD.
-    // This is a refactor. It made the purpose of the function clearer.
-    // There is no need to support converting from any currency to USD in our app, so don't over-engineer at this point.
-    // That is a code smell too, called Speculative Generality. XP warns against this practice with the term YAGNI (You Ain't Going to Need It)
-    fun exchangeToUSD(amountToConvertInJpy: Int): Double {
-        // The conversion logic is clearer now
-        return amountToConvertInJpy / jpyToUsdRate
+    fun exchangeToUSD(amountToConvertInJpy: Int): Double {      // This function exchanges JPY to USD and it is clear from the name what it does.
+        return amountToConvertInJpy / jpyToUsdRate              // The conversion logic is clearer now
     }
+
+    // Note: There is no need to support converting from any currency to USD in our app, so don't over-engineer at this point.
+    // That is a code smell too, called Speculative Generality. XP warns against this practice with the term YAGNI (You Ain't Going to Need It)
 }
