52c893e0a2cb2b72d65bca047fe1c2357b84c1e6
Robert Gravina <rgravina@vmware.com>
2022-07-04 12:36:36 +0900

    Improve the long method example and add notes about other ways to refactor.

--- src/main/kotlin/LongMethod.kt
+++ src/main/kotlin/LongMethod.kt
@@ -1,10 +1,9 @@
-class AccountNotOpenException: Error()
-
 /*
   There are a few steps in this method, but it's not clear where one starts and ends.
   In practice, you may see a lot of code like this. To the person who wrote it at the time, it might look OK.
   But to someone looking at it for the first time, you have to read the whole function to understand what it does.
 */
+class AccountNotOpenException : Error()
 class LongMethodAccount {
     private var open = true
     private var balance = 0
@@ -13,7 +12,7 @@ class LongMethodAccount {
         if (!open) {                                                // Seems like some precondition checking.
             throw AccountNotOpenException()
         }
-        println("customer requested account balance")               // Why is this println here? Was it for debugging?
+        println("acc: customer requested account balance")          // Why is this println here? Was it for debugging?
         if (balance < 0) {                                          // There is some interesting logic here
             // hide this from the customers
             return 0
@@ -40,8 +39,8 @@ class LongMethodAccountRefactored {
         }
     }
 
-    private fun logBalanceRequest() {
-        println("customer requested account balance")
+    private fun logBalanceRequest() {                   // The "acc" prefix seems important for logging, so it could also be extracted to a logging class.
+        println("acc: customer requested account balance")
     }
 
     private fun customerFacingBalance(): Int {          // So now we can see that the logic is to get the customer facing balance
@@ -53,3 +52,9 @@ class LongMethodAccountRefactored {
         return balance
     }
 }
+/*
+  There are many other ways a long method can be refactored. It depends on the reason why the method is long.
+  This example fixes a long method that contains several steps by making the steps clearer using private methods.
+  Some other reason methods might be long include that method does not have a single responsibility, the parameter list
+  might be too long, it contains a long or complex if statement, and many more.
+ */
